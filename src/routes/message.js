8dd5c4cff78f38eff3b7fa0a13b6a639141c5af8
KenshiroKosuke <mahasamud555@gmail.com>
2023-07-21 12:57:57 +0700

    [EDIT] notification option

--- src/routes/message.js
+++ src/routes/message.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const {pushMessages, broadcastMessages} = require('../functions/messageHandler')
 
 router.post("/push", async (req, res, next) => {
-  const {messages,recipientId} = req.body
+  const {messages,recipientId,notificationDisabled} = req.body
   try {
-    const result = await pushMessages(recipientId,messages)
+    const result = await pushMessages(recipientId,messages,notificationDisabled??false)
     res.send(result)
   } catch (err) {
     next(err);
   }
 });
 router.post("/broadcast", async (req, res, next) => {
-  const {messages} = req.body
+  const {messages,notificationDisabled} = req.body
   try {
-    const result = await broadcastMessages(messages)
+    const result = await broadcastMessages(messages,notificationDisabled??false)
     res.send(result)
   } catch (err) {
     next(err);
